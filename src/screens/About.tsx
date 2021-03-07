5c57a336e41e3a1d83b67f50e10609badfeb09f2
Antoine Kingue <etorefr@gmail.com>
2021-03-07 17:45:58 +0100

    fix: Fix footer & about links

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 
-import aboutStyles from '@styles/modules/About.module.scss'
-import utilsStyles from '@styles/modules/Utils.module.scss'
 import { TeamMember } from '@stores/team-members'
 import TeamMemberCard from '@components/TeamMemberCard'
-import Button from '@components/Button';
+
+import aboutStyles from '@styles/modules/About.module.scss'
+import utilsStyles from '@styles/modules/Utils.module.scss'
+import buttonStyles from '@styles/modules/Button.module.scss';
 
 interface Props {
     teamMembers: TeamMember[];
@@ -16,6 +17,14 @@ const About = ({ teamMembers }: Props) => {
     const router = useRouter();
     const { locale } = router;
     const t = require(`../translations/${locale}`).default;
+
+    const leadersRef = React.createRef<HTMLDivElement>();
+
+    const handleClickToLeaders = () => {
+        leadersRef.current.scrollIntoView({
+            behavior: 'smooth'
+        })
+    }
 
     return (
         <section className={[aboutStyles.about, utilsStyles.container].join(' ')}>
@@ -30,14 +39,14 @@ const About = ({ teamMembers }: Props) => {
                     <h3 className={aboutStyles.subtitle}>{t.about.intro.subtitle.toUpperCase()}</h3>
                     <h1 className={aboutStyles.title}>{t.about.intro.title}</h1>
                     <p className={aboutStyles.desc}>{t.about.intro.desc}</p>
-                    <Button to='#leaders' className={aboutStyles.btn}>{t.about.btn}</Button>
+                    <button className={[buttonStyles.button, aboutStyles.btn].join(' ')} onClick={handleClickToLeaders}>{t.about.btn}</button>
                 </div>
                 <div className={aboutStyles.col}>
                     <img className={aboutStyles.img} src="./assets/img/logo/onruntime-transparent-white.png" alt="onRuntime Logo" />
                 </div>
             </div>
-            <div className={aboutStyles.team}>
-                <h3 className={aboutStyles.title} id="#leaders">{t.about.team.title}</h3>
+            <div className={aboutStyles.team} ref={leadersRef}>
+                <h3 className={aboutStyles.title}>{t.about.team.title}</h3>
                 {teamMembers.map((member: TeamMember, i: number) => <TeamMemberCard key={i} align={i & 1 ? 'right' : 'left'} member={member} />)}
             </div>
         </section>
