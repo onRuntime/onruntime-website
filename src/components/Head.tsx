e307f67dd31028d34306e276619045d2784671ed
Antoine Kingue <etorefr@gmail.com>
2021-05-24 19:36:01 +0200

    ⚡️ add description param to add description from head

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,11 +5,13 @@ import { APPNAME } from "../constants/main";
 interface TitleProps {
     title: string;
     subtitle?: string
+    description?: string
 }
 
-const Head: React.FC<TitleProps> = ({ title, subtitle }: TitleProps) => (
+const Head: React.FC<TitleProps> = ({ title, subtitle, description }: TitleProps) => (
     <Helmet>
         {title && <title>{`${subtitle ? `${subtitle} - ` : ""}${title} | ${APPNAME}`}</title>}
+        {description && <meta name="description" content={description} />}
     </Helmet>
 );
 
