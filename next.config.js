3b7b8f603835bb6ba4d19b05830a28b3ab4fda0d
Antoine Kingue <etorefr@gmail.com>
2021-06-17 21:33:36 +0200

    🔧 update next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
+/* eslint-disable no-undef */
 module.exports = {
   i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en'
+    locales: ["en", "fr"],
+    defaultLocale: "en",
   },
-  images: {
-    domains: ['picsum.photos'],
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }
-}
+};
