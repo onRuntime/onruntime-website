4f349b0ec912b7a5d14dd3e13d564809125f69ba
Antoine Kingue <etorefr@gmail.com>
2021-06-17 22:01:40 +0200

    👽️ update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,3 @@
-import About from '@screens/About';
-import { getTeamMembers } from '@services/team';
-import { TeamMember } from '@stores/team-members';
-import { GetStaticProps } from 'next';
+import About from "@screens/About";
 
 export default About;
-
-export const getStaticProps: GetStaticProps = async () => {
-    const teamMembers: TeamMember[] = getTeamMembers();
-
-    return {
-        props: {
-            teamMembers
-        }
-    }
-};
