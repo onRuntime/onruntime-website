a891183da1d55f7a3b3109f388090adec38a1aeb
Antoine Kingue <etorefr@gmail.com>
2021-06-17 22:00:34 +0200

    ✨ add app in pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
-import Layout from '@components/Layout'
+import themes from "@services/themes";
+import GlobalStyle from "@components/Layout/GlobalStyle";
+import Footer from "@components/Footer";
+import Navbar from "@components/Navbar";
 
-import 'remixicon/fonts/remixicon.css'
-import '@styles/app.scss'
+import "remixicon/fonts/remixicon.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <Layout>
+    <ThemeProvider theme={themes.dark}>
+      <GlobalStyle />
+      <Navbar />
       <Component {...pageProps} />
-    </Layout>
-  )
-}
+      <Footer />
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
