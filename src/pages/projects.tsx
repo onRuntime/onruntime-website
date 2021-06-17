69d7185b63406325873fa3077bd09c0cb60a3236
Antoine Kingue <etorefr@gmail.com>
2021-06-17 22:02:24 +0200

    👽️ update projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,14 +1,3 @@
-import Projects from '@screens/Projects'
-import { getSortedProjectsData } from '@services/projects'
-import { GetStaticProps } from 'next'
+import Projects from "@screens/Projects";
 
-export default Projects
-
-export const getStaticProps: GetStaticProps = async () => {
-    const projects = getSortedProjectsData()
-    return {
-        props: {
-            data: projects
-        }
-    }
-}
+export default Projects;
