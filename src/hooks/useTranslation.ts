ced83b4b8074282424632a2d6c5eea14e88aef6d
Antoine Kingue <etorefr@gmail.com>
2021-06-17 21:57:00 +0200

    ✨ add usetranslation hook

---
+++ src/hooks/useTranslation.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { useRouter } from "next/router";
+
+const useTranslation = (
+  namespace = "common"
+): { t: any; locale: string | undefined } => {
+  const router = useRouter();
+  const { locale } = router;
+  const translation = require(`../locales/${locale}/${namespace}`);
+  const translate = (key: string) => {
+    const keyList = key.split(".");
+    let parent = translation;
+    keyList.forEach((w) => (parent = parent[w] ?? key));
+    return parent || key;
+  };
+
+  return { t: translate, locale };
+};
+
+export default useTranslation;
