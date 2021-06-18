d4fab5d84d0a28be9faa321c115f6f4cef439b7c
Antoine Kingue <etorefr@gmail.com>
2021-06-18 13:36:38 +0200

    🏷️ add maintheme to themes type

--- src/types/themes.ts
+++ src/types/themes.ts
@@ -20,3 +20,38 @@ export type Theme = {
     accent: { light: string };
   };
 };
+
+export type MainTheme = {
+  family: {
+    primary: string;
+  };
+  weight: {
+    thin: number;
+    extraLight: number;
+    light: number;
+    regular: number;
+    medium: number;
+    semiBold: number;
+    bold: number;
+    extraBold: number;
+    black: number;
+  };
+  size: {
+    title: string;
+    large: string;
+    medium: string;
+    normal: string;
+    small: string;
+    tiny: string;
+  };
+  breakpoint: {
+    /* Desktops and laptops */
+    desktop: string;
+    /* iPads (landscape) */
+    laptop: string;
+    /* iPads (portrait) */
+    tablet: string;
+    /* Smartphones (landscape) */
+    mobile: string;
+  };
+};
