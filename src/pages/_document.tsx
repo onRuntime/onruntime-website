404a7f3bb0344f68e0f61d6114653f3de285ae29
Antoine Kingue <etorefr@gmail.com>
2021-06-19 04:16:00 +0200

    🩹 fix href paths

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,19 +54,19 @@ export default class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/static/favicon/apple-touch-icon.png"
+            href="/static/images/favicon/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicon/favicon-32x32.png"
+            href="/static/images/favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicon/favicon-16x16.png"
+            href="/static/images/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/manifest.json" />
         </Head>
