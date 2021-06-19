5a9b564257085bdcdc5c1f1f57631d5dc3d46828
Antoine Kingue <etorefr@gmail.com>
2021-06-19 04:58:07 +0200

    🔍 update open graph image path

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -18,29 +18,29 @@ const Head: React.FC<IHead> = ({ title, subtitle, description }: IHead) => {
       <title>{finalTitle}</title>
       {description && (
         <>
-          <meta name="description" content={description} />
-          <meta property="og:description" content={description} />
-          <meta name="twitter:description" content={description} />
+          <meta name={"description"} content={description} />
+          <meta property={"og:description"} content={description} />
+          <meta name={"twitter:description"} content={description} />
         </>
       )}
-      <meta name="keywords" content="onruntime, runtime, studio" />
+      <meta name={"keywords"} content={"onruntime, runtime, studio"} />
 
-      <meta property="og:site_name" content={APPNAME} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://onruntime.com" />
-      <meta property="og:title" content={APPNAME} />
+      <meta property={"og:site_name"} content={APPNAME} />
+      <meta property={"og:type"} content="website" />
+      <meta property={"og:url"} content="https://onruntime.com" />
+      <meta property={"og:title"} content={APPNAME} />
       <meta
-        property="og:image"
-        content="/static/images/logo/onruntime-white-background.jpg"
+        property={"og:image"}
+        content={"/static/images/open-graph-image.jpg"}
       />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:url" content="https://onruntime.com" />
-      <meta name="twitter:site" content="@onRuntime" />
-      <meta name="twitter:title" content={APPNAME} />
+      <meta name={"twitter:card"} content={"summary_large_image"} />
+      <meta name={"twitter:url"} content={"https://onruntime.com"} />
+      <meta name={"twitter:site"} content={"@onRuntime"} />
+      <meta name={"twitter:title"} content={APPNAME} />
       <meta
-        name="twitter:image"
-        content="/static/images/open-graph-image.jpg"
+        name={"twitter:image"}
+        content={"/static/images/open-graph-image.jpg"}
       />
     </NextHead>
   );
