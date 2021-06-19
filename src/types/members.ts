01083e4c206af2d90f5ef7e1e3c90a5faeddf2cb
Younes BESSA <bessa460@gmail.com>
2021-06-19 12:02:57 +0200

    🏷️ update member type

--- src/types/members.ts
+++ src/types/members.ts
@@ -1,8 +1,8 @@
 export type Member = {
-    firstname: string;
-    lastname: string;
-    job: string;
-    thumbnail_url: string;
-    external_url: string;
-    joined_at: string;
-}
+  firstname: string;
+  lastname: string;
+  job: string;
+  thumbnail_url: string;
+  external_url: string;
+  joined_at: string;
+};
