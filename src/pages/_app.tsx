af1ca5f19d369510c690eb4773cf55110f50e7af
Antoine Kingue <etorefr@gmail.com>
2021-06-19 04:17:59 +0200

    ✨ add basic head to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,18 @@ import themes from "@services/themes";
 import GlobalStyle from "@components/Layout/GlobalStyle";
 import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
+import Head from "@components/Head";
 
 import "remixicon/fonts/remixicon.css";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ThemeProvider theme={themes.dark}>
+      <Head
+        description={
+          "onRuntime is a French-Canadian studio that brings together teams of creators and develops applications, websites and game platforms."
+        }
+      />
       <GlobalStyle />
       <Navbar />
       <Component {...pageProps} />
