3847963eaceab28182e5cbc7101f7a62786821fb
Antoine Kingue <etorefr@gmail.com>
2021-07-14 17:28:08 +0200

    🍱 update members url

--- src/data/members.ts
+++ src/data/members.ts
@@ -6,7 +6,7 @@ const members: Member[] = [
     lastname: "Baudrin",
     job: "main.roles.president.cto",
     thumbnail_url: "/static/images/members/jeremy-baudrin.jpg",
-    external_url: "https://linkedin.com/in/jeremybdn/",
+    external_url: "https://linkedin.com/in/jeremybdn",
     joined_at: "2015/01/01",
     // format: yyyy-mm-dd
   },
@@ -15,7 +15,7 @@ const members: Member[] = [
     lastname: "Kingue",
     job: "main.roles.president.ceo",
     thumbnail_url: "/static/images/members/antoine-kingue.jpg",
-    external_url: "https://linkedin.com/in/antoinekm/",
+    external_url: "https://linkedin.com/in/antoinekm",
     joined_at: "2015/01/01",
   },
 ];
