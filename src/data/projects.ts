1172b48c742d1a9144a6ad5b23ca59b799aff757
Antoine Kingue <etorefr@gmail.com>
2022-01-19 23:25:40 +0100

    🍱 update projects and comment unactive projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,62 +1,63 @@
+import ROUTES from "@constants/routes";
 import { Project } from "src/types/project";
 
 const projects: Project[] = [
-  {
-    title: "Twitter Smooth Extension",
-    desc: "projects.list.twittersmooth.desc",
-    link: "https://github.com/onRuntime/twitter-smooth-extension",
-    thumbnail_url: "/static/images/projects/twitter-smooth-extension.jpg",
-    tags: ["Open Source", "Extension"],
-  },
+  // {
+  //   title: "Twitter Smooth Extension",
+  //   desc: "projects.list.twittersmooth.desc",
+  //   link: "https://github.com/onRuntime/twitter-smooth-extension",
+  //   thumbnail_url: "/static/images/projects/twitter-smooth-extension.jpg",
+  //   tags: ["Open Source", "Extension"],
+  // },
   {
     title: "Instagram Dark Extension",
     desc: "projects.list.instagramdark.desc",
-    link: "https://github.com/onRuntime/instagram-dark-extension",
+    link: ROUTES.PROJECTS_INSTAGRAM_DARK,
     thumbnail_url: "/static/images/projects/instagram-dark-extension.jpg",
-    tags: ["Open Source", "Extension", "+20k downloads"],
+    tags: ["Open Source", "Extension", "+40k downloads"],
   },
-  {
-    title: "Clock App",
-    desc: "projects.list.clockapp.desc",
-    link: "https://github.com/onRuntime/clock-app",
-    thumbnail_url: "/static/images/projects/clockapp.webp",
-    tags: ["Open Source"],
-  },
-  {
-    title: "UltraD.JS",
-    desc: "projects.list.ultradjs.desc",
-    link: "https://github.com/onRuntime/ultrad.js",
-    thumbnail_url: "/static/images/projects/ultradjs.jpg",
-    tags: ["Open Source", "Discord"],
-  },
-  {
-    title: "RVBY",
-    desc: "projects.list.rvby.desc",
-    link: "https://rvby.store/",
-    thumbnail_url: "/static/images/projects/rvby.jpg",
-    tags: ["Closed Source"],
-  },
-  {
-    title: "NetflixAddicts",
-    desc: "projects.list.netflixaddicts.desc",
-    link: "https://github.com/onRuntime?q=netflixaddicts",
-    thumbnail_url: "https://picsum.photos/seed/picsum/1920/1080",
-    tags: ["Open Source"],
-  },
-  {
-    title: "BerryGames",
-    desc: "projects.list.berrygames.desc",
-    link: "https://github.com/onRuntime/?q=berrygames",
-    thumbnail_url: "/static/images/projects/berrygames.png",
-    tags: ["Open Source", "Minecraft"],
-  },
-  {
-    title: "FaruGames",
-    desc: "projects.list.farugames.desc",
-    link: "https://github.com/onRuntime/?q=farugames",
-    thumbnail_url: "/static/images/projects/farugames.jpg",
-    tags: ["Open Source", "Minecraft"],
-  },
+  // {
+  //   title: "Clock App",
+  //   desc: "projects.list.clockapp.desc",
+  //   link: "https://github.com/onRuntime/clock-app",
+  //   thumbnail_url: "/static/images/projects/clockapp.webp",
+  //   tags: ["Open Source"],
+  // },
+  // {
+  //   title: "UltraD.JS",
+  //   desc: "projects.list.ultradjs.desc",
+  //   link: "https://github.com/onRuntime/ultrad.js",
+  //   thumbnail_url: "/static/images/projects/ultradjs.jpg",
+  //   tags: ["Open Source", "Discord"],
+  // },
+  // {
+  //   title: "RVBY",
+  //   desc: "projects.list.rvby.desc",
+  //   link: "https://rvby.store/",
+  //   thumbnail_url: "/static/images/projects/rvby.jpg",
+  //   tags: ["Closed Source"],
+  // },
+  // {
+  //   title: "NetflixAddicts",
+  //   desc: "projects.list.netflixaddicts.desc",
+  //   link: "https://github.com/onRuntime?q=netflixaddicts",
+  //   thumbnail_url: "https://picsum.photos/seed/picsum/1920/1080",
+  //   tags: ["Open Source"],
+  // },
+  // {
+  //   title: "BerryGames",
+  //   desc: "projects.list.berrygames.desc",
+  //   link: "https://github.com/onRuntime/?q=berrygames",
+  //   thumbnail_url: "/static/images/projects/berrygames.png",
+  //   tags: ["Open Source", "Minecraft"],
+  // },
+  // {
+  //   title: "FaruGames",
+  //   desc: "projects.list.farugames.desc",
+  //   link: "https://github.com/onRuntime/?q=farugames",
+  //   thumbnail_url: "/static/images/projects/farugames.jpg",
+  //   tags: ["Open Source", "Minecraft"],
+  // },
 ];
 
 export default projects;
