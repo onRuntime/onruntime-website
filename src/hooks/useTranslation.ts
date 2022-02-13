154678862bcf694bfd812a8ba5dda1859a8bc3f6
Antoine Kingue <etorefr@gmail.com>
2022-02-13 17:01:14 +0100

    ⚡️ improve usetranslation logic

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -3,14 +3,46 @@ import { useRouter } from "next/router";
 
 const useTranslation = (
   namespace = "common"
-): { t: any; locale: string | undefined } => {
+): {
+  t: (key: string, variables?: string[]) => any;
+  locale: string | undefined;
+} => {
   const router = useRouter();
-  const { locale } = router;
-  const translation = require(`../locales/${locale}/${namespace}`);
-  const translate = (key: string) => {
+  const { locale, locales } = router;
+  let translation: any;
+  try {
+    translation = require(`../locales/${locale}/${namespace}.json`);
+  } catch (err) {
+    console.error(
+      `An error occurred while loading the translation file: ${namespace}`
+    );
+  }
+
+  if (!translation) {
+    try {
+      translation = require(`../locales/${
+        locales && locales.length > 0 ? locales[0] : "en"
+      }/${namespace}.json`);
+    } catch (err) {
+      console.error(
+        `An error occurred while loading the fallback translation file: ${namespace}`
+      );
+    }
+  }
+
+  const translate = (key: string, variables: string[] = []) => {
     const keyList = key.split(".");
     let parent = translation;
-    keyList.forEach((w) => (parent = parent[w] ?? key));
+    keyList.forEach((k) => (parent = parent[k] ?? key));
+
+    // replace %s with variables
+    if (variables.length > 0) {
+      const regex = new RegExp("%s", "g");
+      return parent.replace(regex, (match: any) => variables.shift() ?? match);
+    }
+
+    console.log(`handle translation for ${key} => ${parent}`);
+
     return parent || key;
   };
 
