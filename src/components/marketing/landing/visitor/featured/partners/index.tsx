c11d0c9d9ad6f5fd2528bdec7db326c6bf783c80
Antoine Kingue <etorefr@gmail.com>
2025-02-14 00:05:10 +0100

    🔀 merge pull request #127 from onRuntime/antoinekm/partners
    🍱 add new partner images for enhanced visual content

--- src/components/marketing/landing/visitor/featured/partners/index.tsx
+++ src/components/marketing/landing/visitor/featured/partners/index.tsx
@@ -1,14 +1,28 @@
-import { OnRuntimeWordMark } from "@/logos/components";
+import Image from "next/image";
 import React from "react";
 
 const Partners: React.FC = () => {
   return (
     <div className="mt-12">
-      <p className="text-center">Ces entreprises nous font confiance</p>
+      <p className="text-center">Ils nous accompagnent</p>
 
-      <div className="mt-14 overflow-hidden">
-        <div className="flex space-x-8">
-          <OnRuntimeWordMark height={24} />
+      <div className="mt-8 overflow-hidden">
+        <div className="flex items-center justify-center space-x-16">
+          <Image
+            src="/static/images/partners/frenchtechnormandy.png"
+            alt="French Tech Normandy"
+            width={100}
+            height={100}
+            className="h-20 w-20 object-contain grayscale transition-all duration-300 hover:grayscale-0"
+          />
+
+          <Image
+            src="/static/images/partners/needforschool.png"
+            alt="Need For School"
+            width={100}
+            height={100}
+            className="h-20 w-20 object-contain grayscale transition-all duration-300 hover:grayscale-0"
+          />
         </div>
       </div>
     </div>
