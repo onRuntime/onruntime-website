e1cc09880a74f09e0626a6059dedda7e6420104d
Antoine Kingue <etorefr@gmail.com>
2025-02-13 22:25:56 +0100

    🎨 improve text with balancer for improved readability

--- src/components/marketing/landing/visitor/featured/index.tsx
+++ src/components/marketing/landing/visitor/featured/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import React from "react";
 import Partners from "./partners";
 import { OnRuntimeWordMark } from "@/logos/components";
+import Balancer from "react-wrap-balancer";
 
 const Featured: React.FC = () => {
   return (
@@ -12,19 +13,21 @@ const Featured: React.FC = () => {
       <div className="px-4 md:px-0 py-16 flex flex-col justify-center items-center max-w-5xl mx-auto">
         <div className="relative max-w-xl flex flex-col items-center gap-6">
           <h1 className="font-medium text-5xl md:text-6xl text-foreground text-center">
-            La machine à rêves,{" "}
-            <span className="text-onruntime-blue inline">onRuntime Studio</span>
-            .
+            <Balancer>
+              La machine à rêves, {" "}
+              <span className="text-onruntime-blue inline">onRuntime Studio</span>.
+            </Balancer>
           </h1>
 
           <h2 className="text-center text-muted-foreground">
-            onRuntime Studio cartonne grâce à nos groupes de créateurs :
-            Développeurs, designers et artistes de tous horizons
+            <Balancer>
+              Une communauté dynamique de créateurs : développeurs, designers et
+              artistes de divers horizons, unis par l'innovation et la créativité.
+            </Balancer>
           </h2>
 
           <div className="flex gap-3">
             <Button>Réserver une prestation</Button>
-
             <Button variant={"outline"}>Voir notre travail</Button>
           </div>
 
@@ -57,13 +60,15 @@ const Featured: React.FC = () => {
             />
 
             <p className="hidden md:block text-background leading-5">
-              À la base, nous sommes passionnés par le développement web et le
-              développement en général depuis plus de 6 ans. Nous sommes très
-              intéressés par les nouvelles technologies et le monde de la
-              création (design, musique, etc.). À partir de là nous avons créé
-              un Studio et une communauté autour de celui ci, nous adorons nous
-              retrouver autour d&apos;une tasse de café ☕️ pour discuter, jouer et
-              surtout créer des projets.
+              <Balancer>
+                À la base, nous sommes passionnés par le développement web et le
+                développement en général depuis plus de 6 ans. Nous sommes très
+                intéressés par les nouvelles technologies et le monde de la
+                création (design, musique, etc.). À partir de là nous avons créé
+                un Studio et une communauté autour de celui-ci, nous adorons nous
+                retrouver autour d&apos;une tasse de café ☕️ pour discuter, jouer et
+                surtout créer des projets.
+              </Balancer>
             </p>
 
             <OnRuntimeWordMark fill="white" height={24} className="self-end" />
