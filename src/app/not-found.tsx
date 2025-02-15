8aa22461f46913a1640c0ad2b3ddf212e7b4a1b1
Antoine Kingue <etorefr@gmail.com>
2025-02-15 05:17:39 +0100

    ✨ add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <section className="h-[100vh] flex items-center justify-center flex-col gap-4">
+      <h1 className="text-2xl font-semibold tracking-tighter">
+        404 - Page Not Found
+      </h1>
+      <p>The page you are looking for does not exist.</p>
+    </section>
+  )
+}
