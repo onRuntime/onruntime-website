705bb3b31a4a829ccaa2f21d57ffbc6ef52a34a7
jerembdn <work@jeremybdn.fr>
2025-02-21 21:47:31 +0100

    🔧 update navbar padding for improved layout

--- src/components/marketing/navbar/index.tsx
+++ src/components/marketing/navbar/index.tsx
@@ -7,7 +7,7 @@ import Navigation from "./navigation";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="px-4 md:px-0 z-50 fixed w-full max-w-4xl mt-2.5">
+    <nav className="px-7 md:px-0 z-50 fixed w-full max-w-4xl mt-2.5">
       <div className="flex justify-between items-center p-2.5 bg-background/50 rounded-lg shadow-xs backdrop-blur-2xl">
         <Link href={Routes.landing.visitor}>
           <OnRuntimeWordMark height={24} />
