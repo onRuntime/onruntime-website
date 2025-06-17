a3b6fb48d95e57471e50d257f62b36e990bd5901
louis Lazare <louislazare.pro@gmail.com>
2025-06-17 09:10:05 +0200

    ✅  [UPDATE] : adding a new role + new user
    [NEW ROLE : TRAINEE DEVELOPER]
    [NEW USER : Louis LAZARE]

--- src/types/team-member.ts
+++ src/types/team-member.ts
@@ -3,6 +3,7 @@ export enum TeamRole {
   LEAD_DEVELOPER = "lead-developer",
   LEAD_DESIGNER = "lead-designer",
   DEVELOPER = "developer",
+  TRAINEE_DEVELOPER = "trainee-developer",
   DESIGNER = "designer",
   MARKETING = "marketing",
   COMMUNITY_MANAGER = "community-manager",
@@ -13,6 +14,7 @@ export enum TeamRole {
 export const roleToDisplay: Record<TeamRole, string> = {
   [TeamRole.CO_FOUNDER]: "Co-Fondateur",
   [TeamRole.DEVELOPER]: "Développeur",
+  [TeamRole.TRAINEE_DEVELOPER]: "Développeur Stagiaire",
   [TeamRole.LEAD_DEVELOPER]: "Lead Développeur",
   [TeamRole.LEAD_DESIGNER]: "Lead Designer",
   [TeamRole.DESIGNER]: "Designer",
