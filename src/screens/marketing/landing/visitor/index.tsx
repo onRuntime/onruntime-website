13b6ec738f3885b92db7204f5936cfb230e7c57a
Asma <asma.derragui@hotmail.fr>
2025-06-23 11:23:32 +0200

    :heavy_plus_sign: added tonightpass section to the homepage

--- src/screens/marketing/landing/visitor/index.tsx
+++ src/screens/marketing/landing/visitor/index.tsx
@@ -1,6 +1,7 @@
 import Featured from "@/components/marketing/landing/visitor/featured";
 import Projects from "@/components/marketing/landing/visitor/projects";
 import Team from "@/components/marketing/landing/visitor/team";
+import Tonightpass from "@/components/marketing/landing/visitor/tonightpass";
 import type { NextPage } from "next";
 import React from "react";
 
@@ -8,6 +9,7 @@ const VisitorLanding: NextPage = () => {
   return (
     <>
       <Featured />
+      <Tonightpass />
       <Team />
       <Projects />
     </>
