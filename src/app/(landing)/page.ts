6e2505ef4507adb490753be48cb2a9bc53c4bd54
Jérémy Cailly <jeremy.cailly@hotmail.fr>
2025-06-23 11:21:14 +0200

    🔍 add canonical link to homepage

--- src/app/(landing)/page.ts
+++ src/app/(landing)/page.ts
@@ -4,6 +4,7 @@ import VisitorLanding from "@/screens/marketing/landing/visitor";
 export const metadata = constructMetadata({
   title: "Agence web de développement et design UI/UX à Paris et Rouen",
   description: "Agence digitale spécialisée en développement web, mobile et design UI/UX. Notre équipe d'experts transforme vos idées en solutions digitales performantes.",
+  canonical: "https://onruntime.com/",
 });
 
 export default VisitorLanding;
