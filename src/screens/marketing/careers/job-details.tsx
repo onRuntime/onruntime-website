8c1266f6b2ff531c9698ae7f3494e6b6972a8b75
Antoine Kingue <etorefr@gmail.com>
2025-07-07 08:34:24 +0200

    ✏️ fix html entity for apostrophes in job detail page

--- src/screens/marketing/careers/job-details.tsx
+++ src/screens/marketing/careers/job-details.tsx
@@ -233,9 +233,9 @@ const JobDetailPage: React.FC<JobDetailPageProps> = ({ job }) => {
             </h2>
             <p className="text-muted-foreground mb-6">
               Chez onRuntime Studio, nous cultivons un environnement de travail
-              collaboratif et stimulant où chaque membre de l'équipe peut
+              collaboratif et stimulant où chaque membre de l&apos;équipe peut
               exprimer sa créativité et développer ses compétences. Nous
-              privilégions l'innovation, la qualité et le bien-être de nos
+              privilégions l&apos;innovation, la qualité et le bien-être de nos
               collaborateurs.
             </p>
 
