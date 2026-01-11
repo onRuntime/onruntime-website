7c42d1c4dda81e96ece3611d71c69abc539bb4f8
Antoine Kingue <etorefr@gmail.com>
2026-01-11 16:51:02 +0100

    ♻️ update web app sitemap to use automatic page discovery

--- apps/web/src/app/sitemap.xml/[id]/route.ts
+++ apps/web/src/app/sitemap.xml/[id]/route.ts
@@ -4,14 +4,10 @@ import { localeCodes, defaultLocale } from "@/lib/translations";
 
 export const dynamic = "force-static";
 
-// @ts-expect-error - require.context is a webpack/turbopack feature
-const pagesContext = require.context("../../[locale]", true, /\/page\.tsx$/);
-
 const { generateStaticParams, GET } = createSitemapHandler({
   baseUrl: siteConfig.url,
   locales: localeCodes,
   defaultLocale,
-  pagesContext,
 });
 
 export { generateStaticParams, GET };
